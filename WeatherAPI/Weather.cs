3f060041747452e2473ab6a5dbae4426b6d75708
harry790717 <57786265+harry790717@users.noreply.github.com>
2019-11-21 22:52:29 +0800

    Add files via upload

---
+++ WeatherAPI/Weather.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WeatherAPI.Models.Request
+{
+    public class WeatherRequest
+    {
+        public string CityNo { get; set; }
+        public string StartTime { get; set; }
+        public string EndTime { get; set; }
+    }
+}
