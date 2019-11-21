b8d492c6d0440e1cd9ca901a8d03f9f1c94ed94e
harry790717 <57786265+harry790717@users.noreply.github.com>
2019-11-21 22:45:08 +0800

    Add files via upload

---
+++ WeatherController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WeatherAPI.Models.Request;
+using WeatherAPI.Models.Response;
+using WeatherAPI.Models.Repository;
+using WeatherAPI.DLL;
+using Newtonsoft.Json;
+
+namespace WeatherAPI.Controllers
+{
+    public class WeatherController : ApiController
+    {
+        // POST api/transfernumberapi
+        public string Post([FromBody]string requestStr)
+        {
+            try
+            {
+                
+                WeatherRequest requests = JsonConvert.DeserializeObject<WeatherRequest>(requestStr);
+                string output = "";
+                
+                WeatherRepository repository = new WeatherRepository();
+                WeatherResult results = repository.Check(requests);
+                output = JsonConvert.SerializeObject(results);
+
+                return output;
+            }
+            catch (Exception ex)
+            {
+                //發生錯誤時，傳回HTTP 500及錯誤訊息
+                var resp = new HttpResponseMessage()
+                {
+                    StatusCode = HttpStatusCode.InternalServerError,
+                    Content = new StringContent(ex.Message),
+                    ReasonPhrase = "Web API Error"
+                };
+                throw new HttpResponseException(resp);
+            }
+        }
+    }
+}
---
+++ WeatherRepository.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WeatherAPI.Models.Request;
+using WeatherAPI.Models.Response;
+using WeatherAPI.DLL;
+using System.Data;
+
+namespace WeatherAPI.Models.Repository
+{
+    public class WeatherRepository
+    {
+        private WeatherResult weather = new WeatherResult();
+        private List<WeatherResultList> weatherlist = new List<WeatherResultList>();
+        public WeatherResult Check(WeatherRequest requests)
+        {
+            List<WeatherResult> results = new List<WeatherResult>();
+            string ErrStr = "";
+            string ResultStr = "";
+
+            if (string.IsNullOrEmpty(requests.CityNo))
+            {
+                ErrStr = "CityNo為必填欄位";
+            }
+            if (string.IsNullOrEmpty(requests.StartTime))
+            {
+                ErrStr = "StartTime為必填欄位";
+            }
+            if (string.IsNullOrEmpty(requests.EndTime))
+            {
+                ErrStr = "EndTime為必填欄位";
+            }
+
+            if (ErrStr == "")
+            {
+                WeatherDAO TransferCheck = new WeatherDAO();
+                DataTable dt = TransferCheck.Search(requests);
+                if (dt.Rows.Count > 0)
+                {
+                    for (int i = 0; i < dt.Rows.Count; i++)
+                    {
+                        Add(new WeatherResultList
+                        {
+                            Result = dt.Rows[i]["Result"].ToString()
+                        });
+                    }
+                    weather.ResultList = weatherlist;
+
+                }
+                else
+                {
+                    ResultStr = "該區間查無資料";
+                }
+            }
+            else
+            {
+                ResultStr = ErrStr;
+            }
+            weather.StatusCode = 1;
+            weather.ResultStr = ErrStr;
+
+            return weather;
+        }
+        public WeatherResultList Add(WeatherResultList item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException("item");
+            }
+
+            weatherlist.Add(item);
+            return item;
+        }
+    }
+}
