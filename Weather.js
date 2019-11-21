230c62475e768364232e4ed60a31681b25c2153d
harry790717 <57786265+harry790717@users.noreply.github.com>
2019-11-21 22:32:57 +0800

    Update Weather.js

--- Weather.js
+++ Weather.js
@@ -1,27 +1,25 @@
-const { URL } = require('url');
-const request = require('request');
-var apikey = 'CWB-5E206CD9-B2A9-4FEE-876E-16A29F495FDA';
+const https = require('https');
 
-
-function get(CityNo,cb) {
-	var url = new URL('http://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?&locationName=' + CityNo);//'&format=JSON'
-    request.get({
-        "headers": { "content-type": "application/json","Authorization": 'CWB-5E206CD9-B2A9-4FEE-876E-16A29F495FDA'},
-        "url": url   
-    }, (error, response, body) => {
-        if (error) {
-            console.dir(error);
-            cb(false,null);
-        }
-        var pdata = JSON.parse(body);
-        cb(pdata.records.location[0].weatherElement,pdata.records.location[0].locationName);
-        
-    });
+function GetWeatherData(CityNo,cb) {
+	var options = {
+		host: 'opendata.cwb.gov.tw',
+		method: 'GET',
+		path: '/api/v1/rest/datastore/F-C0032-001?&locationName=' + encodeURI(CityNo),
+		headers: {
+		'content-type': 'application/json',
+		'Authorization': 'CWB-5E206CD9-B2A9-4FEE-876E-16A29F495FDA'
+		}
+	};
+	var req = https.get(options, (res) => {
+	res.on('data', (d) => {
+		var pdata = JSON.parse(d);	
+		cb(pdata.records.location[0].weatherElement,pdata.records.location[0].locationName);	
+		});
+	}).on('error', (e) => {
+		console.error(e);
+	});
 }
 
-
- 
-// [START exports]
 module.exports = { 
-    get,
-}; 
+    GetWeatherData,
+};
